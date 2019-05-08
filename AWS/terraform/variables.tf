cad052be9c5dd7be6e4ab75c332e6caeb351d3c4
Dirk Fuchs <difu@mac.com>
2019-05-08 18:08:35 +0200

    Python3 print function

---
+++ AWS/terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "eu-central-1"
+}
+
+variable "project" {
+  description = "The name of this project"
+  default     = "PiWeather"
+}
