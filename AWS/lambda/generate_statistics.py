7733c5fe0133350c5fa69315bad6a131b43f85d5
difu <difu@mac.com>
2020-06-10 23:15:47 +0200

    median and mean calculations

--- AWS/lambda/generate_statistics.py
+++ AWS/lambda/generate_statistics.py
@@ -4,7 +4,7 @@ sys.path.append("/opt/")
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 import json
-
+import statistics
 import datetime
 import dateutil.tz
 
@@ -12,11 +12,12 @@ dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
 table = dynamodb.Table('WeatherData')
 
 
-def generate_daily_stats(start_date, days, force=False):
+def generate_daily_stats(sensor, start_date, days, force=False):
     """
 
-    :param start_date:
-    :param days:
+    :param sensor: the sensor
+    :param start_date: date in iso format
+    :param days: number of days beginning from start_date
     :param force: if True the statistics will be re-calculated, if False existing values will not be changed
     :return:
     """
@@ -26,36 +27,52 @@ def generate_daily_stats(start_date, days, force=False):
 
     for i in range(days):
         the_date = datetime.datetime.strptime(start_date, date_format) + datetime.timedelta(days=i)
-        print(the_date.strftime("%Y-%m-%dT"))
+        date_only = the_date.strftime("%Y-%m-%dT")
+        print(date_only)
 
         response = table.query(
-            KeyConditionExpression=Key('Sensor').eq('Sensor/lacrosse/44/T') & Key('Timestamp').begins_with(the_date.strftime("%Y-%m-%dT"))
+            KeyConditionExpression=Key('Sensor').eq(sensor) & Key('Timestamp').begins_with(date_only)
         )
         count = 0
         values = []
         max_ts = ""
         min_ts = ""
-        for i in response['Items']:
+        for item in response['Items']:
             # print("Count %s  Bla %s", str(count), i['payload'])
-            value = i['payload']['value'][0]
+            value = item['payload']['value'][0]
             values.append(value)
             if value >= max(values):
-                max_ts = i['Timestamp']
+                max_ts = item['Timestamp']
             if value <= min(values):
-                min_ts = i['Timestamp']
+                min_ts = item['Timestamp']
             count = count + 1
 
         print("Count:")
         print(count)
         print("Max")
-        print(max(values))
+        max_value = max(values)
+        print(max_value)
         print(max_ts)
 
         print("Min")
-        print(min(values))
+        min_value = min(values)
+        print(min_value)
         print(min_ts)
+
+        print("median")
+        median_value = statistics.median(values)
+        print(median_value)
+        print("mean")
+        mean_value = statistics.mean(values)
+        print(mean_value)
+
+        # response = table.update_item(
+        #
+        #     ReturnValues="UPDATED_NEW"
+        # )
+        # print(response)
 
 
 if __name__ == "__main__":
     # lambda_handler('','')
-    generate_daily_stats('2020-05-31T', 1)
+    generate_daily_stats('Sensor/lacrosse/44/T', '2020-05-31T', 1)
